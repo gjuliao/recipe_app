dde449d4ef1a9aa16b010af5b97dfa011f543bf2
Giovanni Juliao <gjuliao32@gmail.com>
2023-04-17 14:33:53 -0500

    Added cancan generator

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root "articles#index"
+
   resources :users do
     resources :recipes
     resources :foods do
@@ -8,5 +10,4 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
 end
