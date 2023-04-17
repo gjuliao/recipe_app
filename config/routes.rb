5497a074ce68c3d3bec49d0ee4501b34cd859e7e
Giovanni Juliao <gjuliao32@gmail.com>
2023-04-17 14:28:36 -0500

    Added routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
-  resources :recipe_foods
-  resources :recipes
-  resources :foods
-  resources :users
+  resources :users do
+    resources :recipes
+    resources :foods do
+      resources :recipe_foods
+    end
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
