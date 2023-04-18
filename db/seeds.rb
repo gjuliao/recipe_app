2ead8981e8d93c31b8b0c82766340a73b801c7dd
Victor <victorgmassimi.microverse@gmail.com>
2023-04-18 09:45:27 -0600

    added seed to database

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+
+user = User.create(name: 'John')
+recipe1 = Recipe.create(name: 'Pizza', preparation_time: 60, cooking_time: 50, description: 'This is a pizza recipe', public: true, user: user)
+recipe2 = Recipe.create(name: 'Pasta', preparation_time:30, cooking_time: 40, description: 'This is a pasta recipe', public: false, user: user)
+food1 = Food.create(name: 'apple', measurement_unit: 'grams', price: 1.9, quantity: 5, user: user)
+food2 = Food.create(name: 'orange', measurement_unit: 'grams', price: 5.0, quantity: 2, user: user)
+food3 = Food.create(name: 'avocado', measurement_unit: 'grams', price: 9.99, quantity: 1, user: user)
