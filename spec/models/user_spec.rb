d7a71e02671a2cdc7737f961c96156cbcaa9d337
Giovanni Juliao <gjuliao32@gmail.com>
2023-04-21 08:07:10 -0500

    Check for presence of fields in User

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -21,10 +21,30 @@ RSpec.describe User, type: :model do
   end
 
   describe "devise modules" do
-    let(:user) { User.create(name: 'John', email: 'john@gmail.com', password: '1231231') }
-
-    it "should be database authenticatable" do
+    it "should have valid password" do
       expect(user.valid_password?('1231231')).to be true
+    end
+
+    it "Name should have MIN length" do
+      user1 = User.create(name: 'J', email: 'john@hotmail.com', password: '1231231')
+      expect(user1.errors[:name]).to include("is too short (minimum is 3 characters)")
+    end
+
+    it "Name should have MAX length" do
+      user1 = User.create(name: 'Frank Sinatra' * 10, email: 'john@hotmail.com', password: '1231231')
+      expect(user1.errors[:name]).to include("is too long (maximum is 50 characters)")
+    end
+
+    it "Presence of Password" do
+      user1 = User.create(name: 'Frank Sinatra', email: 'jhon@email.com', password: '')
+      expect(user1).to_not be_valid
+      expect(user1.errors[:password]).to include("can't be blank")
+    end
+
+    it "Presence of Email" do
+      user1 = User.create(name: 'Frank Sinatra', email: '', password: '1231231')
+      expect(user1).to_not be_valid
+      expect(user1.errors[:email]).to include("can't be blank")
     end
 
     it "should include database_authenticatable in the list of devise modules" do
